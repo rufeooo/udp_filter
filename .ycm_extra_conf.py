78bca63d43a5f5efc6de34b963e581c5783e2209
Alan Newton <rufeooo@gmail.com>
2019-01-27 06:38:33 -0800

    Code navigation config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,4 +1,7 @@
-def FlagsForFile(*args):
-  print(args)
-  return { "flags" : ["-O0"],
-      "do_cache" : False }
+# YouCompleteMe default project settings
+def Settings( **kwargs ):
+  return {
+      'flags': ['-O0', '-I/usr/include/'],
+      'include_paths_relative_to_dir': '.',
+      }
+
